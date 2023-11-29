0aece2a09edaa18a6dd15b15d829f011db7b5491
Fernando Buligon <fbuligonantunes@gmail.com>
2023-11-29 10:48:59 -0300

    correção bug imagem not initialized

--- lib/banco_de_dados.dart
+++ lib/banco_de_dados.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+//
 class bancodeDados {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
